07420be0778a7716fefee938a4a4c21a1e4904d5
Arpit Choudhary <144100310+Yoshi-Sama-lab@users.noreply.github.com>
2026-08-30 23:23:31 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.java
@@ -0,0 +1,62 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result=new ArrayList<>();
+        char[][] board=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        class Solver{
+            void backtrack(int row){
+                if(row==n){
+                    List<String> inner =new ArrayList<>();
+                    for(int i =0;i<n;i++){
+                        inner.add(new String(board[i]));
+                    }
+                    result.add(inner);
+                    return;
+                }
+                for(int col=0;col<n;col++){
+                    boolean isSafe=true;
+                    for(int i=0;i<row;i++){
+                        if(board[i][col]=='Q'){
+                            isSafe=false;
+                            break;
+                        }
+                    }
+                    if(isSafe){
+                        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+                            if(board[i][j]=='Q'){
+                                isSafe=false;
+                                break;
+                        }
+                    }
+                    }
+                    
+                    if(isSafe){
+                        for(int i=row-1,j=col+1;i>=0&&j<n;i--,j++){
+                            if(board[i][j]=='Q'){
+                                isSafe=false;
+                                break;
+                        }
+                    }
+                    }
+                    
+                    if(isSafe){
+                        board[row][col]='Q';
+                        backtrack(row+1);
+                        board[row][col]='.';
+                    }
+                        
+                    
+
+                }
+                
+            }
+        }
+        new Solver().backtrack(0);
+        return result;
+        
+    }
+}
